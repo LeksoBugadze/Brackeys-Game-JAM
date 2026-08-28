5116ae8e6eb789f6b419614c9a57a04aeb954b6f
vakje <man263170@gmail.com>
2026-08-28 16:39:32 +0400

    fixed the sizing problem range is functioning correctly

--- Scripts/Buffs/slash_buffs/slashRDC_buff.gd
+++ Scripts/Buffs/slash_buffs/slashRDC_buff.gd
@@ -7,9 +7,17 @@ extends Node
 @export var cooldown: float = 1.5
 func _ready() -> void:
 	#somehow add range first simply make the slash bigger
+	print(parent_node.scale)
+	parent_node.scale = Vector3(2,1,2)
+	print(parent_node.scale)
 	print(parent_node.gpu_particles_3d.scale)
-	parent_node.gpu_particles_3d.scale = Vector3(10,10,10)
-	print(parent_node.gpu_particles_3d.scale)
-	parent_node.collision_shape_3d.scale = Vector3(4,2.17,4)
+	var mat = parent_node.gpu_particles_3d.process_material as ParticleProcessMaterial
+	if mat:
+		# original min max is 1.8
+		mat.scale_min = 3.6
+		mat.scale_max = 3.6
+		
+		# restarting the particles to force the gpu to load it with my sacling properties
+		parent_node.gpu_particles_3d.restart() 
 	parent_node.slash_damage = damage
 	parent_node.timer.wait_time = cooldown
